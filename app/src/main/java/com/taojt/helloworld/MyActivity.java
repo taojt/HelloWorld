b9a7e6c7a73790f117504fef62e4ab9b256e2e7c
taojt <taojt08@163.com>
2016-07-20 13:24:38 +0800

    hello world

--- app/src/main/java/com/taojt/helloworld/MyActivity.java
+++ app/src/main/java/com/taojt/helloworld/MyActivity.java
@@ -1,18 +1,21 @@
 package com.taojt.helloworld;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MyActivity extends AppCompatActivity {
+public class MyActivity extends AppCompatActivity{
     private EditText editText;
     private Button sendButton;
     public static String EXTRA_MESSAGE = "com.taojt.helloworld.MESSAGE";
@@ -37,8 +40,9 @@ public class MyActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_my, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -46,15 +50,28 @@ public class MyActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch(item.getItemId()){
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+
         }
-
-        return super.onOptionsItemSelected(item);
+        
     }
+
+    private void openSettings() {
+    }
+
+    private void openSearch() {
+    }
+
     public void sendMessage(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
