28d31c55544f3b0f6051c16df8666d27ac00ef68
taojt <taojt08@163.com>
2016-07-20 11:27:31 +0800

    add files

--- app/src/main/java/com/taojt/helloworld/MyActivity.java
+++ app/src/main/java/com/taojt/helloworld/MyActivity.java
@@ -1,5 +1,6 @@
 package com.taojt.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button sendButton;
+    public static String EXTRA_MESSAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +54,12 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
